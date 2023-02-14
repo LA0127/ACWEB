3c7810d42ec05879eb6e38f244cfd69d4f01f42f
LA0127 <la.contreras.h@edu.utc.mx>
2023-02-13 18:26:29 -0600

    Tarea

--- 05EjerciciosJS/EJ10/JS/ej10.js
+++ 05EjerciciosJS/EJ10/JS/ej10.js
@@ -1,18 +1,19 @@
-function calculate() {
-    let salary = parseFloat(document.querySelector('#salary').value);
-    let seniority = parseFloat(document.querySelector('#seniority').value);
-    let percentage;
-    if (seniority < 1) {
-      percentage = 0.05;
-    } else if (seniority >= 1 && seniority < 2) {
-      percentage = 0.07;
-    } else if (seniority >= 2 && seniority < 5) {
-      percentage = 0.10;
-    } else if (seniority >= 5 && seniority < 10) {
-      percentage = 0.15;
+function calcularUtilidad() {
+    var antiguedad = parseFloat(document.getElementById("antiguedad").value);
+    var salario = parseFloat(document.getElementById("salario").value);
+    var utilidad;
+    
+    if (antiguedad < 1) {
+      utilidad = salario * 0.05;
+    } else if (antiguedad >= 1 && antiguedad < 2) {
+      utilidad = salario * 0.07;
+    } else if (antiguedad >= 2 && antiguedad < 5) {
+      utilidad = salario * 0.10;
+    } else if (antiguedad >= 5 && antiguedad < 10) {
+      utilidad = salario * 0.15;
     } else {
-      percentage = 0.20;
+      utilidad = salario * 0.20;
     }
-    let annualUtility = salary * percentage * 12;
-    document.querySelector('#result').innerHTML = `La utilidad anual es de $${annualUtility.toFixed(2)}`;
+    
+    document.getElementById("resultado").innerHTML = "La utilidad es de $" + utilidad.toFixed(2);
   }
